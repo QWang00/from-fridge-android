564f6bcdd87d618e3d0c3724e1c4c864687bfed3
qiuqiu <qiujuanwang87@gmail.com>
2025-06-10 15:05:12 +0100

    feat: create ApiService with endpoints

---
+++ app/src/main/java/com/recipes/from_fridge/service/ApiService.java
@@ -0,0 +1,35 @@
+package com.recipes.from_fridge.service;
+
+import com.recipes.from_fridge.model.FridgeIngredient;
+import com.recipes.from_fridge.model.Recipe;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface ApiService {
+
+    @GET("fridge/ingredients")
+    Call<List<FridgeIngredient>> getAllFridgeIngredients();
+
+    @POST("fridge/ingredient")
+    @FormUrlEncoded
+    Call<FridgeIngredient> addIngredientToFridge(@Field("ingredient") String ingredientName);
+
+    @DELETE("fridge/ingredient/{id}")
+    Call<String> removeIngredientFromFridge(@Path("id") Integer id);
+
+    @DELETE("fridge/ingredients")
+    Call<String> clearFridge();
+
+
+    @GET("recipes/search")
+    Call<List<Recipe>> searchRecipesByIngredients(
+            @Query("ingredientNames") List<String> ingredientNames
+    );
+
+    @GET("recipes/{id}/detail")
+    Call<Recipe> getRecipeDetail(
+            @Path("id") Integer recipeId,
+            @Query("matchedIngredients") List<String> matchedIngredients
+    );
+}
