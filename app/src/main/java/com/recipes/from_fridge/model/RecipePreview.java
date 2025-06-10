a52c4dc2f99cdd1e86594d5b962ba5672009a1a9
qiuqiu <qiujuanwang87@gmail.com>
2025-06-10 16:39:10 +0100

    feat: create RecipePreview DTO in model package

---
+++ app/src/main/java/com/recipes/from_fridge/model/RecipePreview.java
@@ -0,0 +1,25 @@
+package com.recipes.from_fridge.model;
+
+import java.util.List;
+
+public class RecipePreview {
+    private String title;
+    private String imageUrl;
+    private int matchedCount;
+    private List<String> matchedIngredients;
+
+    public RecipePreview() {}
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+
+    public int getMatchedCount() { return matchedCount; }
+    public void setMatchedCount(int matchedCount) { this.matchedCount = matchedCount; }
+
+    public List<String> getMatchedIngredients() { return matchedIngredients; }
+    public void setMatchedIngredients(List<String> matchedIngredients) { this.matchedIngredients = matchedIngredients; }
+}
+
