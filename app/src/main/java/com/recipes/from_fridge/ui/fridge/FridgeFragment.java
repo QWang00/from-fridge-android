75e5564e03bf76fb070abce24f81f71586ee15d5
qiuqiu <qiujuanwang87@gmail.com>
2025-06-12 13:06:29 +0100

    feat: add FridgeFragment to display fridge contents

--- app/src/main/java/com/recipes/from_fridge/ui/fridge/FridgeFragment.java
+++ app/src/main/java/com/recipes/from_fridge/ui/fridge/FridgeFragment.java
@@ -1,28 +1,35 @@
 package com.recipes.from_fridge.ui.fridge;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import com.recipes.from_fridge.databinding.FragmentFridgeBinding;
 
 public class FridgeFragment extends Fragment {
-
+    private FridgeViewModel viewModel;
     private FragmentFridgeBinding binding;
+    private FridgeAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        FridgeViewModel fridgeViewModel =
+       viewModel =
                 new ViewModelProvider(this).get(FridgeViewModel.class);
 
         binding = FragmentFridgeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        binding.setViewModel(viewModel);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
 
-        return root;
+        setupRemoveIngredientRecyclerView();
+        setupFab();
+        observeViewModel();
+        viewModel.loadFridgeIngredients();
+        return binding.getRoot();
     }
 
     @Override
@@ -30,4 +37,40 @@ public class FridgeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    private void setupRemoveIngredientRecyclerView(){
+        adapter = new FridgeAdapter(
+                ingredient -> {
+            new AlertDialog.Builder(requireContext())
+                    .setTitle("Remove Fridge Ingredient").setMessage("Are you sure you want to remove \"" + ingredient.getName() + "\"from your fridge?")
+            .setPositiveButton("Yes", (dialog, which)->{
+                viewModel.removeIngredientFromFridge(ingredient);
+            })
+                    .setNegativeButton("Cancel", (dialog, which)->{
+                        dialog.dismiss();
+                    })
+                    .show();
+        },
+
+            FridgeAdapter.ActionMode.REMOVE
+        );
+
+        binding.myFridgeRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.setAdapter(adapter);
+    }
+
+    private void observeViewModel(){
+        viewModel.getFridgeIngredients().observe(getViewLifecycleOwner(), adapter::setFridgeIngredients);
+    }
+
+    private void setupFab() {
+        binding.fabAddIngredient.setOnClickListener(v -> {
+            // TODO: 跳转到添加页面
+            // 如果你用 Navigation Component:
+            // NavHostFragment.findNavController(this).navigate(R.id.action_fridgeFragment_to_searchFragment);
+        });
+    }
+
+
+
 }
