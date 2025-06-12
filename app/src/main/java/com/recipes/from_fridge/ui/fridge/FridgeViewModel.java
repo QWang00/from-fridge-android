d0935d6d638a873d44c4781879798e6606dc22aa
qiuqiu <qiujuanwang87@gmail.com>
2025-06-12 11:06:28 +0100

    feat: add FridgeViewModel to manage fridge ingredients

--- app/src/main/java/com/recipes/from_fridge/ui/fridge/FridgeViewModel.java
+++ app/src/main/java/com/recipes/from_fridge/ui/fridge/FridgeViewModel.java
@@ -3,17 +3,52 @@ package com.recipes.from_fridge.ui.fridge;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.recipes.from_fridge.repository.FridgeIngredientRepository;
+import com.recipes.from_fridge.model.FridgeIngredient;
+import com.recipes.from_fridge.model.Ingredient;
+import java.util.List;
 
 public class FridgeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final FridgeIngredientRepository repository;
 
-    public FridgeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is fridge fragment");
+    private final MutableLiveData<List<FridgeIngredient>> fridgeIngredients = new MutableLiveData<>();
+
+    public FridgeViewModel(FridgeIngredientRepository repository) {
+        this.repository = repository;
+        loadFridgeIngredients();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<FridgeIngredient>> getFridgeIngredients() {
+        return fridgeIngredients;
+    }
+
+    public void loadFridgeIngredients() {
+        repository.getFridgeIngredients().observeForever(fetched -> {
+            fridgeIngredients.setValue(fetched);
+        });
+    }
+
+    public void removeIngredientFromFridge(Ingredient ingredient) {
+        repository.removeIngredientFromFridge(
+                ingredient.getId(),
+                this::loadFridgeIngredients,
+                () -> {}
+        );
+    }
+
+    public void addIngredientToFridge(String name) {
+        repository.addToFridge(
+                name,
+                this::loadFridgeIngredients,
+                () -> {}
+        );
+    }
+
+    public void clearFridge() {
+        repository.clearFridge(
+                this::loadFridgeIngredients,
+                () -> {}
+        );
     }
 }
