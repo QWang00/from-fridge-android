b655490c71284375ea309b4887b4789f41c041e1
qiuqiu <qiujuanwang87@gmail.com>
2025-06-16 11:52:52 +0100

    feat: create SearchIngredientAdapter for displaying ingredient list

---
+++ app/src/main/java/com/recipes/from_fridge/ui/fridge/SearchIngredientAdapter.java
@@ -0,0 +1,64 @@
+package com.recipes.from_fridge.ui.fridge;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.recipes.from_fridge.databinding.ItemIngredientSearchBinding;
+import com.recipes.from_fridge.model.Ingredient;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchIngredientAdapter extends RecyclerView.Adapter<SearchIngredientAdapter.ViewHolder> {
+
+    public interface OnAddClickListener {
+        void onAddClick(Ingredient ingredient);
+    }
+
+    private List<Ingredient> ingredientList = new ArrayList<>();
+    private final OnAddClickListener listener;
+
+    public SearchIngredientAdapter(OnAddClickListener listener) {
+        this.listener = listener;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredientList = ingredients;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemIngredientSearchBinding binding = ItemIngredientSearchBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Ingredient ingredient = ingredientList.get(position);
+        holder.bind(ingredient, listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredientList != null ? ingredientList.size() : 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ItemIngredientSearchBinding binding;
+
+        public ViewHolder(ItemIngredientSearchBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(Ingredient ingredient, OnAddClickListener listener) {
+            binding.setIngredient(ingredient);
+            binding.setClickListener(listener);
+            binding.executePendingBindings();
+        }
+    }
+}
