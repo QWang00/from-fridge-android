1745b63fade34fd616a71f26886a89ace7ef021f
qiuqiu <qiujuanwang87@gmail.com>
2025-06-16 12:15:59 +0100

    feat: implement setupSearchBox in AddIngredientFragment

---
+++ app/src/main/java/com/recipes/from_fridge/ui/fridge/AddIngredientFragment.java
@@ -0,0 +1,58 @@
+package com.recipes.from_fridge.ui.fridge;
+
+import static java.security.AccessController.getContext;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import com.recipes.from_fridge.databinding.FragmentAddIngredientBinding;
+
+public class AddIngredientFragment extends Fragment {
+    private FragmentAddIngredientBinding binding;
+    private AddIngredientViewModel viewModel;
+    private SearchIngredientAdapter adapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentAddIngredientBinding.inflate(inflater, container, false);
+        viewModel = new ViewModelProvider(this).get(AddIngredientViewModel.class);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+
+        setupRecyclerView();
+        setupSearchBox();
+
+        return binding.getRoot();
+    }
+
+    private void setupRecyclerView() {
+        adapter = new SearchIngredientAdapter(ingredient -> {
+
+        });
+
+        binding.searchIngredientRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.searchIngredientRecyclerView.setAdapter(adapter);
+
+        viewModel.getSearchResults().observe(getViewLifecycleOwner(), adapter::setIngredients);
+    }
+
+    private void setupSearchBox() {
+        binding.etSearch.addTextChangedListener(new TextWatcher() {
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+                viewModel.searchIngredients(s.toString());
+            }
+
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override public void afterTextChanged(Editable s) {}
+        });
+    }
+}
