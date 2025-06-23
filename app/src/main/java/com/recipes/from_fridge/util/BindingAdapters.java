59a69cdf799db27134efa88e45210fcc1b9e395a
qiuqiu <qiujuanwang87@gmail.com>
2025-06-23 14:39:38 +0100

    feat: implement loadImage using Glide in BindingAdapters

--- app/src/main/java/com/recipes/from_fridge/util/BindingAdapters.java
+++ app/src/main/java/com/recipes/from_fridge/util/BindingAdapters.java
@@ -1,7 +1,11 @@
 package com.recipes.from_fridge.util;
 
+import android.widget.ImageView;
 import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.recipes.from_fridge.R;
 
 public class BindingAdapters {
 
@@ -10,5 +14,15 @@ public class BindingAdapters {
         if (recyclerView.getAdapter() != adapter) {
             recyclerView.setAdapter(adapter);
         }
+    }
+
+    @BindingAdapter("imageUrl")
+    public static void loadImage(ImageView view, String imageUrl) {
+        Glide.with(view.getContext())
+                .load(imageUrl)
+                .placeholder(R.drawable.ic_placeholder_ingredient)
+                .error(R.drawable.ic_placeholder_ingredient)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
     }
 }
