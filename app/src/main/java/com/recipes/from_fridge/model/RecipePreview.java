52de724648a7df87c5677379dddb578be544d934
qiuqiu <qiujuanwang87@gmail.com>
2025-06-25 12:14:22 +0100

    feat: add id field to RecipePreview

--- app/src/main/java/com/recipes/from_fridge/model/RecipePreview.java
+++ app/src/main/java/com/recipes/from_fridge/model/RecipePreview.java
@@ -3,12 +3,16 @@ package com.recipes.from_fridge.model;
 import java.util.List;
 
 public class RecipePreview {
+    private Integer id;
     private String title;
     private String imageUrl;
     private int matchedCount;
     private List<String> matchedIngredients;
 
     public RecipePreview() {}
+
+    public Integer getId(){return id;};
+    public void setId(){this.id = id;}
 
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
